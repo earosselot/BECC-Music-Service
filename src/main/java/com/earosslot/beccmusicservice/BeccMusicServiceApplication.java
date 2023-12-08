6ee2dae7a710bd9c932016553ba68c9c3952f4d6
earosselot <earosselot@gmail.com>
2023-12-08 16:44:05 +0100

    projet initialization

---
+++ src/main/java/com/earosslot/beccmusicservice/BeccMusicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.earosslot.beccmusicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeccMusicServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeccMusicServiceApplication.class, args);
+    }
+
+}
