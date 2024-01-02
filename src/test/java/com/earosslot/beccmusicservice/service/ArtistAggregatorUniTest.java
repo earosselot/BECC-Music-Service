e5a0594d63ee5355145e1defd681ccd683e7f75c
Eduardo Rosselot <earosselot@gmail.com>
2024-01-02 14:54:38 +0100

    - improved agregator paralellization cases

--- src/test/java/com/earosslot/beccmusicservice/service/ArtistAggregatorUniTest.java
+++ src/test/java/com/earosslot/beccmusicservice/service/ArtistAggregatorUniTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -34,69 +35,50 @@ class ArtistAggregatorUniTest {
     @DisplayName("01. given artist request, when has one album, description and album lookout are parallelized")
     void testParallelApiCalls1() throws InterruptedException {
 
-        Set<Long> threadIds = new HashSet<>();
-
         int numberOfAlbums = 1;
-        CountDownLatch latch = new CountDownLatch(numberOfAlbums + 1);
-
-        when(musicBrainz.getArtist("MBID")).thenReturn(artistWithNumberOfAlbums(numberOfAlbums));
-
-        doAnswer(invocation -> {
-            System.out.println("Thread ID: " + Thread.currentThread().getId());
-            threadIds.add(Thread.currentThread().getId());
-            latch.countDown();
-            return null; // For void methods
-        }).when(coverArtArchiveService).fillImageLink(any());
-
-        doAnswer(invocation -> {
-            System.out.println("Thread ID: " + Thread.currentThread().getId());
-            threadIds.add(Thread.currentThread().getId());
-            latch.countDown();
-            return null;
-        }).when(wikipediaService).fillDescription(any());
+        Set<Long> threadIds = new HashSet<>();
+        CountDownLatch latch = mockAlbumsResponseAndThreadCounter(numberOfAlbums, threadIds);
 
         ArtistAggregator artistAggregator = new ArtistAggregator(musicBrainz, wikipediaService, coverArtArchiveService);
         artistAggregator.getArtist("MBID");
 
         // Wait for all threads to finish
         latch.await();
-
         verify(coverArtArchiveService, times(numberOfAlbums)).fillImageLink(any());
         assertTrue(threadIds.size() > 1);
     }
 
-
-    @Test
-    @DisplayName("01. given artist request, when has more than one album, the description and album requests are parallelized")
-    void testParallelApiCalls() throws InterruptedException {
-
-        Set<Long> threadIds = new HashSet<>();
-
-        int numberOfAlbums = 4;
+    private CountDownLatch mockAlbumsResponseAndThreadCounter(int numberOfAlbums, Set<Long> threadIds) {
         CountDownLatch latch = new CountDownLatch(numberOfAlbums + 1);
-
         when(musicBrainz.getArtist("MBID")).thenReturn(artistWithNumberOfAlbums(numberOfAlbums));
+        doAnswer(countThread(threadIds, latch)).when(coverArtArchiveService).fillImageLink(any());
+        doAnswer(countThread(threadIds, latch)).when(wikipediaService).fillDescription(any());
+        return latch;
+    }
 
-        doAnswer(invocation -> {
+    private static Answer countThread(Set<Long> threadIds, CountDownLatch latch) {
+        return invocation -> {
             System.out.println("Thread ID: " + Thread.currentThread().getId());
             threadIds.add(Thread.currentThread().getId());
             latch.countDown();
             return null; // For void methods
-        }).when(coverArtArchiveService).fillImageLink(any());
+        };
+    }
 
-        doAnswer(invocation -> {
-            System.out.println("Thread ID: " + Thread.currentThread().getId());
-            threadIds.add(Thread.currentThread().getId());
-            latch.countDown();
-            return null;
-        }).when(wikipediaService).fillDescription(any());
+
+    @Test
+    @DisplayName("02. given artist request, when has more than one album, the description and album requests are parallelized")
+    void testParallelApiCalls() throws InterruptedException {
+
+        Set<Long> threadIds = new HashSet<>();
+        int numberOfAlbums = 4;
+        CountDownLatch latch = mockAlbumsResponseAndThreadCounter(numberOfAlbums, threadIds);
 
         ArtistAggregator artistAggregator = new ArtistAggregator(musicBrainz, wikipediaService, coverArtArchiveService);
         artistAggregator.getArtist("MBID");
 
         // Wait for all threads to finish
         latch.await();
-
         verify(coverArtArchiveService, times(numberOfAlbums)).fillImageLink(any());
         assertTrue(threadIds.size() > 1);
     }
